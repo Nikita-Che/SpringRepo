3ba50973a3922fb4c315e07d388d9a47d785af10
nikita <nikitachertkov@gmail.com>
2022-09-14 15:32:42 +0500

    testChanges

--- DI IO MVC REST Tasks/src/main/java/com/example/spring/Test1.java
+++ DI IO MVC REST Tasks/src/main/java/com/example/spring/Test1.java
@@ -4,14 +4,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test1 {
     public static void main(String[] args) {
-
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.tld");
 
         Pet myPet = context.getBean("myPet",Pet.class);
         myPet.say();
-        myPet.say();
 
     }
-
-
 }
