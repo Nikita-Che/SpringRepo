e2aee2b01f118073287c1cfc4a67a1de13563ea5
nikita <nikitachertkov@gmail.com>
2022-09-28 16:41:18 +0500

    add delete uptade added

--- MVC + Hibernate + AOP/p/src/main/java/spring/mvc/controller/MyController.java
+++ MVC + Hibernate + AOP/p/src/main/java/spring/mvc/controller/MyController.java
@@ -3,9 +3,12 @@ package spring.mvc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import spring.mvc.dao.EmployeeDao;
 import spring.mvc.entity.Employee;
+import spring.mvc.service.EmployeeService;
 
 import java.util.List;
 
@@ -13,13 +16,51 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-    private EmployeeDao employeeDao;
+//    private EmployeeDao employeeDao;
+    EmployeeService employeeService;
+
 
     @RequestMapping("/")
     public String showAllEmployees(Model model) {
-        List<Employee> allEmployees = employeeDao.getAllEmployees();
+//        List<Employee> allEmployees = employeeDao.getAllEmployees();
+//        model.addAttribute("allEmps", allEmployees);
+
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
 
         return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+
+        employeeService.deleteEmployee(id);
+
+        return "redirect:/";
     }
 }
--- MVC + Hibernate + AOP/p/src/main/java/spring/mvc/dao/EmployeeDaoImpl.java
+++ MVC + Hibernate + AOP/p/src/main/java/spring/mvc/dao/EmployeeDaoImpl.java
@@ -1,14 +1,12 @@
 package spring.mvc.dao;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import spring.mvc.entity.Employee;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -18,7 +16,7 @@ public class EmployeeDaoImpl implements EmployeeDao {
     private SessionFactory sessionFactory;
 
     @Override
-    @Transactional
+//    @Transactional
     public List<Employee> getAllEmployees() {
 
         Session session = sessionFactory.getCurrentSession();
@@ -28,5 +26,29 @@ public class EmployeeDaoImpl implements EmployeeDao {
 //        List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(employee);
+
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee where id=:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
